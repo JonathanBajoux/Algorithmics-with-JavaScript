21081822b06370a52effe140e086e24a7bcdde50
jonathan <jonathanbajoux.pro@gmail.com>
2022-11-25 16:43:20 +0100

    jonathan

--- exercice2/2.7.js
+++ exercice2/2.7.js
@@ -1,2 +1,12 @@
-let user = prompt('un nombre nommé')
-if(user = n)
+let user = prompt('enter a number named')
+function calculetteSam(user){
+    console.log(user)
+    for(i=1; i<=user; i++){
+       let newEntry = prompt('enter new number') 
+    }
+    console.log()
+    
+
+    
+
+}
